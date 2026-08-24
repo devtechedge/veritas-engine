4e2f650915318076a5b252c4aee7424463450445
devtechedge <devtechedge@gmail.com>
2026-08-24 17:02:13 +0000

    chore: drop unused deps and add typecheck scripts

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   experimental: {
     serverComponentsExternalPackages: ["@langchain/langgraph"],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
